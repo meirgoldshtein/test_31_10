1b76e7804627ec25656fc2badc8f8d1ae21bef70
meirgoldshtein <m0548491057@gmail.com>
2024-10-31 09:28:04 +0200

    The main app component is complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import Add from "./components/Add"
 import Todo from "./models/todoModel"
 import List from "./components/List"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function App() {
 
   const [todos, setTodos] = useState<Todo[]>([])
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const apikey = '8184480'
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`https://reactexambackend.onrender.com/missions/:${apikey}`);
+      const data = await response.json();
+      setTodos((arr :any) => [...arr, ...data]);
+      console.log(data);
+      setIsLoading(false);
+    } catch (error: any) {
+      setError(error.message);
+      setTodos([]);
+      setIsLoading(false);
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className='app'>
       <Add setTodos={setTodos} />
